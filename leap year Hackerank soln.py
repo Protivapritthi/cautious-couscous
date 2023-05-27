93e4d64a0c45aeb6a079e8461dc8eae06db3d218
Protiva Das <dasprotiva319@gmail.com>
2023-05-27 09:57:11 +0600

    Update leap year Hackerank soln.py

--- leap year Hackerank soln.py
+++ leap year Hackerank soln.py
@@ -1,11 +1,16 @@
 def is_leap(year):
+    
+    # variable to check the leap year
     leap = False
-
-    if year % 4 != 0:
-        return leap
-    else:
+    
+    # One if statement that contains multiple conditions
+    if ((year % 400 == 0) and (year % 100 == 0)) | ((year % 4 ==0) and (year % 100 != 0)):
+        
+       # change leap to True
         leap = True
-        return leap
+
+    
+    return leap
 
 
 year = int(input())
